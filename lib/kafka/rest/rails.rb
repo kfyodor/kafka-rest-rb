d647fda877f51f2a318f13e957541130bb0c0856
Theodore Konukhov <komolov.f@gmail.com>
2016-09-01 15:02:15 +0300

    initial

---
+++ lib/kafka/rest/rails.rb
@@ -0,0 +1,9 @@
+require "kafka/rest/rails/version"
+
+module Kafka
+  module Rest
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/kafka/rest/rails/version.rb
@@ -0,0 +1,7 @@
+module Kafka
+  module Rest
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
