fd10bb477a9c4fa477d2103c5737ab301f73fa8e
Theodore Konukhov <komolov.f@gmail.com>
2016-09-12 14:29:13 +0300

    kafka-rest client

--- lib/kafka_rest/client.rb
+++ lib/kafka_rest/client.rb
@@ -1,14 +1,15 @@
 require 'faraday'
-require 'faraday_middleware'
+require 'client/middleware.rb'
 
 module KafkaRest
   class Client
-
     def initialize
       @conn = Faraday.new(url: 'http://localhost:8082') do |c|
-        # c.use :instrumentation#  if config.instrumentation
-
         c.request :json
+        c.request :log_resp
+        c.request :default_headers, default_headers
+
+        c.response :raise_exception
         c.response :json
 
         c.adapter :net_http_persistent
@@ -18,119 +19,117 @@ module KafkaRest
     # Get list of topics
     ### returns: array of topics
     def topics
-      get("/topics")
+      @conn.get("/topics")
     end
 
     # Get topic metadata by name
     ### returns: name, configs, partitions
     def topic(topic)
-      get("/topics/#{topic}")
+      @conn.get("/topics/#{topic}")
     end
 
     # Get topic's partitions
     ###
     def topic_partitions(topic)
-      get("/topics/#{topic}/partitions")
+      @conn.get("/topics/#{topic}/partitions")
     end
 
     # Get topic's partition metadata
     def topic_partition(topic, partition)
-      get("/topics/#{topic}/partitions/#{partition}")
+      @conn.get("/topics/#{topic}/partitions/#{partition}")
     end
 
     # Get messages from topic's partition.
-    def topic_partition_messages(topic, partition, offset, count = 1)
-      params = { offset: offset, count: count }
-      opts =   { headers: accept_header(:avro) } # move this to config (and maybe we'll need per-topic format config)
+    def topic_partition_messages(topic, partition, params = {})
+      params[:count] ||= 1
+      format = params.delete(:format) || 'binary'
 
-      get("/topics/#{topic}/partitions/#{partition}/messages", params, opts)
+      @conn.get(
+        "/topics/#{topic}/partitions/#{partition}/messages",
+        params,
+        accept(format)
+      )
     end
+
+    def produce_message(path, records, format)
+      body = { records: records.is_a?(Array) ? records : [records] }
+      @conn.post(path, body, content_type(format))
+    end
+    private :produce_message
 
     # Produce message into a topic
     ### params: key_schema, value_schema, key_schema_id, value_schema_id, records { key, value, partition }
     ### returns: key_schema_id, value_schema_id, offsets { partition, offset, error_code, error }
-    def topic_produce_message(topic, message)
-      opts = { headers: accept_header.merge(content_type_header(:avro)) }
-      post("topics/#{topic}", message, opts)
+    def topic_produce_message(topic, records, format = 'json')
+      produce_message("topics/#{topic}", records, format)
     end
 
     # Produce message into a topic and partition
     ### see topic_produce_message
-    def topic_partition_produce_message(topic, partition, message)
-      opts = { headers: accept_header.merge(content_type_header(:avro)) }
-      post("topics/#{topic}/partitions/#{partition}", message, opts)
+    def topic_partition_produce_message(topic, partition, records, format = 'json')
+      produce_message("topics/#{topic}/partitions/#{partition}", records, format)
     end
 
     # Add new consumer to a group
     ### params: name, format, auto.offset.reset, auto.commit.enable
     ### returns: instance_id, base_uri
-    def consumer_add(group_name, params)
-      post("consumers/#{group_name}", params, headers: accept_header)
+    def consumer_add(group_name, params = {})
+      body = {}
+      body['auto.offset.reset'] = params[:auth_offset_reset] || 'largest'
+      body['auto.commit.enable'] = params[:auto_commit_enable] == true || false
+      body['format'] = params[:format] || 'json'
+
+      @conn.post("consumers/#{group_name}", body)
     end
 
     def consumer_commit_offsets(group_name, consumer_id)
-      post("consumers/#{group_name}/instances/#{consumer_id}/offsets", nil, headers: accept_header)
+      @conn.post("consumers/#{group_name}/instances/#{consumer_id}/offsets")
     end
 
     def consumer_remove(group_name, consumer_id)
-      delete("consumers/#{group_name}/instances/#{consumer_id}", headers: accept_header)
+      @conn.delete("consumers/#{group_name}/instances/#{consumer_id}")
     end
 
     def consumer_consume_from_topic(group_name, consumer_id, topic, params = {})
-      get(
+      format = params.delete(:format) || 'json'
+
+      @conn.get(
         "consumers/#{group_name}/instances/#{consumer_id}/topics/#{topic}",
         params,
-        headers: accept_header(:avro))
+        accept(format)
+      )
     end
 
     def brokers
-      get("/brokers")
+      @conn.get("/brokers")
     end
 
     private
 
-    def request!(method, path, params, options)
-      @conn.run_request(
-        method.to_sym,
-        path,
-        params,
-        options[:headers]
-      ).body # TODO: error handling
-    end
-
-    def get(path, params = {}, opts = {})
-      request!(:get, path, params, opts)
-    end
-
-    def post(path, params = {}, opts = {})
-      request!(:post, path, params, opts)
-    end
-
-    def delete(path, opts = {})
-      request!(:delete, path, nil, opts)
-    end
-
     def default_headers
-      accept_header
+      {
+        'Accept' => 'application/vnd.kafka.v1+json, application/vnd.kafka+json, application/json',
+        'Content-Type' => 'application/vnd.kafka.v1+json'
+      }
     end
 
-    def mime_type(format = :json)
-      case format
+    def kafka_mime_type(format = :json)
+      case format.to_sym
       when :avro
         'application/vnd.kafka.avro.v1+json'
       when :binary
         'application/vnd.kafka.binary.v1+json'
       when :json
-        'application/vnd.kafka.v1+json'
+        'application/vnd.kafka.json.v1+json'
       end
     end
 
-    def accept_header(format = nil)
-      { 'Accept' => mime_type(format) }
+    def content_type(format = nil, headers = {})
+      headers.merge 'Content-Type' => kafka_mime_type(format)
     end
 
-    def content_type_header(format = nil)
-      { 'Content-Type' => mime_type(format) }
+    def accept(format, headers = {})
+      headers.merge 'Accept' => kafka_mime_type(format)
     end
   end
 end
---
+++ lib/kafka_rest/client/middleware.rb
@@ -0,0 +1,69 @@
+require 'faraday'
+require 'faraday_middleware/response_middleware'
+require 'oj'
+
+module KafkaRest
+  class Client
+    class KafkaRestClientException < StandardError
+      attr_reader :body, :status
+
+      def initialize(resp)
+        @body = resp.body
+        @status = resp.status
+
+        super "#{@body['message']}" +
+              " (HTTP Status: #{@status}; " +
+              "error code: #{@body['error_code']})"
+      end
+    end
+
+    class DefaultHeaders < Faraday::Middleware
+      def initialize(app = nil, default_headers = {})
+        @default_headers = default_headers
+        super(app)
+      end
+
+      def call(env)
+        env[:request_headers] = @default_headers.merge env[:request_headers]
+        @app.call(env)
+      end
+    end
+
+    class JsonRequest < Faraday::Middleware
+      def call(env)
+        if env[:body]
+          env[:body] = Oj.dump env[:body], mode: :compat, symbol_keys: false
+        end
+
+        @app.call(env)
+      end
+    end
+
+    class JsonResponse < FaradayMiddleware::ResponseMiddleware
+      define_parser do |body|
+        Oj.load(body)
+      end
+    end
+
+    class RaiseException < FaradayMiddleware::ResponseMiddleware
+      def call(env)
+        response = @app.call(env)
+        response.on_complete do
+          unless response.success?
+            raise KafkaRestClientException.new(response)
+          end
+        end
+      end
+    end
+
+    Faraday::Request.register_middleware(
+      default_headers: DefaultHeaders,
+      json: JsonRequest
+    )
+
+    Faraday::Response.register_middleware(
+      json: JsonResponse,
+      raise_exception: RaiseException
+    )
+  end
+end
