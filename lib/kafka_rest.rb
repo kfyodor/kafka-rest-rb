d2838e0e4511bfcf50a9e59af51fde2dad30753a
Theodore Konukhov <komolov.f@gmail.com>
2016-09-12 18:26:57 +0300

    binary and gemspec fixes

--- lib/kafka_rest.rb
+++ lib/kafka_rest.rb
@@ -1,7 +1,6 @@
+require 'kafka_rest/worker'
 require 'kafka_rest/config'
 require 'kafka_rest/client'
 require 'kafka_rest/message'
 require 'kafka_rest/consumer'
 require 'kafka_rest/producer'
-
-module KafkaRest; end
